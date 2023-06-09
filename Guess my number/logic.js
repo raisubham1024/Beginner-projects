b5493cb4fd34e764608ffaf9d46b6d485249444c
Subham <raisubham1024@gmail.com>
2023-06-09 12:10:26 +0530

    {[Modify] logic of Guess number} : guessing limitation added

--- Guess my number/logic.js
+++ Guess my number/logic.js
@@ -1,8 +1,11 @@
 let randomNumber = Math.ceil(Math.random() * 100)
+let valueInput_times = 0
+let limit = 2
 console.log(randomNumber)
 let userInput
 
-while (userInput != randomNumber) {
+while (userInput != randomNumber && valueInput_times !== limit) {
+  valueInput_times++
   if (userInput > randomNumber) {
     console.log("Greater than")
   } else if (userInput < randomNumber) {
@@ -11,7 +14,9 @@ while (userInput != randomNumber) {
 
   userInput = Number(prompt("Enter any value : "))
 }
-
+if (valueInput_times == 0) {
+  console.log(`Your chances over !!`)
+}
 if (userInput === randomNumber) {
   console.log("Correct Number")
 }
